22711c8542051ce216fcd5a23815148ca987c215
LekoArts <lekoarts@gmail.com>
2020-01-24 12:21:51 +0100

    Edit homepage to not use MDX files

--- src/@lekoarts/gatsby-theme-minimal-blog/components/post.js
+++ src/@lekoarts/gatsby-theme-minimal-blog/components/post.js
@@ -1,9 +1,9 @@
 /** @jsx jsx */
-import { jsx, Styled } from "theme-ui"
-import React from "react"
-import Layout from "@lekoarts/gatsby-theme-minimal-blog/src/components/layout"
-import ItemTags from "@lekoarts/gatsby-theme-minimal-blog/src/components/item-tags"
-import SEO from "@lekoarts/gatsby-theme-minimal-blog/src/components/seo"
+import { jsx, Styled } from 'theme-ui'
+import React from 'react'
+import Layout from '@lekoarts/gatsby-theme-minimal-blog/src/components/layout'
+import ItemTags from '@lekoarts/gatsby-theme-minimal-blog/src/components/item-tags'
+import SEO from '@lekoarts/gatsby-theme-minimal-blog/src/components/seo'
 
 const px = [`32px`, `16px`, `8px`, `4px`]
 const shadow = px.map(v => `rgba(0, 0, 0, 0.15) 0px ${v} ${v} 0px`)
@@ -16,7 +16,14 @@ const Post = ({ data: { post } }) => (
       image={post.banner ? post.banner.childImageSharp.resize.src : undefined}
     />
     <Styled.h2>{post.title}</Styled.h2>
-    <p sx={{ color: `secondary`, mt: 3, a: { color: `secondary` }, fontSize: [1, 1, 2] }}>
+    <p
+      sx={{
+        color: `secondary`,
+        mt: 3,
+        a: { color: `secondary` },
+        fontSize: [1, 1, 2],
+      }}
+    >
       <time>{post.date}</time>
       {post.tags && (
         <React.Fragment>
@@ -27,8 +34,16 @@ const Post = ({ data: { post } }) => (
       {` — `}
       <span>{post.timeToRead} min read</span>
     </p>
-    <section sx={{ my: 5, ".gatsby-resp-image-wrapper": { my: [4, 4, 5], boxShadow: shadow.join(`, `) } }}>
-    {post.body}
+    <section
+      sx={{
+        my: 5,
+        '.gatsby-resp-image-wrapper': {
+          my: [4, 4, 5],
+          boxShadow: shadow.join(`, `),
+        },
+      }}
+    >
+      {post.body}
     </section>
   </Layout>
 )
